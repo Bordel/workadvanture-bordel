d4dcc5f67daf2ab0754a27f62604135e2ba40cb7
taxmeifyoucan <61149543+taxmeifyoucan@users.noreply.github.com>
2020-12-29 17:57:35 +0000

    Deploying to gh-pages from master @ 55e9218fff9e5af89611e4744430300f0d79f59e 🚀

--- hackatoshi.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="hackatoshi" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../hackatosh.jpg" width="100" height="100"/>
-</tileset>
--- tilesets_deviant_milkian_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../workadventure-polis/tilesets_deviant_milkian_1.png" width="512" height="512"/>
-</tileset>
