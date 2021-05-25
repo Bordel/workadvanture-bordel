8bad94c4ffa23d8f545aea6f63ee29ec61d2a046
taxmeifyoucan <mario.havel@protonmail.com>
2021-05-25 12:31:32 +0100

    add pacman

--- pacman.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="pacman" tilewidth="32" tileheight="32" tilecount="392" columns="28">
- <image source="pacman.png" width="900" height="469"/>
-</tileset>
